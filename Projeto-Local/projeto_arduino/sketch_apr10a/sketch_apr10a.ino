5a0d0ba2c6aa96dbea483f09d1a24c60ccc21d8f
Aluno <Aluno@Digital.net>
2019-04-10 17:44:16 -0300

    adicionado biblioteca e codigo arduino

---
+++ Projeto-Local/projeto_arduino/sketch_apr10a/sketch_apr10a.ino
@@ -0,0 +1,25 @@
+#include <dht.h>
+
+const int pinoDHT11 = A5; //PINO ANALÓGICO UTILIZADO PELO DHT11
+
+dht DHT; //VARIÁVEL DO TIPO DHT
+
+void setup(){
+  Serial.begin(9600); //INICIALIZA A SERIAL
+  delay(2000); //INTERVALO DE 2 SEGUNDO ANTES DE INICIAR
+}
+
+void loop(){
+  DHT.read11(pinoDHT11); //LÊ AS INFORMAÇÕES DO SENSOR
+
+  Serial.print(DHT.temperature); //IMPRIME NA SERIAL O VALOR DE UMIDADE MEDIDO E REMOVE A PARTE DECIMAL
+  Serial.print(","); //ESCREVE O TEXTO EM SEGUIDA
+
+ Serial.println(DHT.humidity); //IMPRIME NA SERIAL O VALOR DE UMIDADE MEDIDO
+
+
+
+  
+  delay(1000); //INTERVALO DE 2 SEGUNDOS * NÃO DIMINUIR ESSE VALOR
+}
+
