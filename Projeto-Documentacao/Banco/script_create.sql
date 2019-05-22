fcf7806ee679f3fbffc60b87fb5b3a30a96a25f7
Segundo Usuario <ede>
2019-05-22 17:53:16 -0300

    älterado o insert do node local

--- Projeto-Documentacao/Banco/script_create.sql
+++ Projeto-Documentacao/Banco/script_create.sql
@@ -7,13 +7,21 @@ Tipo_Alerta varchar(1),
 Tipo_Unidade varchar(1)
 ); 
 
+create table Unidade(
+idUnidade int primary key,
+endereco varchar(45),
+nome varchar(35)
+);
+
 create table Sensor (
 idSensor int primary key,
 Modelo_sensor varchar(30),
 Fk_idAlerta int,
-apelido varchar(40)
+apelido varchar(40),
+fk_unidade int
 );
 alter table Sensor add foreign key (Fk_idAlerta) references Alerta(idAlerta); 
+alter table Sensor add foreign key (fk_unidade) references Unidade(idUnidade); 
 
 create table Medicamentos (
 idMedicamentos int primary key, 
@@ -28,11 +36,15 @@ idAtual int primary key,
 Temperatura_Atual float, 
 Umidade_Atual float,
 Data_mon datetime,
-fk_Sensor int);
+fk_Sensor int,
+media float,
+mediana float,
+Pquart float,
+Tquart float);
 
 alter table Monitoramento add foreign key(fk_Sensor) references Sensor(idSensor);
 
-create table Usuario (CPF int primary key,
+create table Usuario (CPF bigint primary key,
 Nome_completo varchar(40),
 Nome_usuario varchar(30),
 Email varchar(35),
@@ -40,11 +52,22 @@ Senha varchar(20),
 Tel_Cel varchar(15),
 CRF varchar(25), 
 Administrador bit,
-Notificacao bit);
+Notificacao bit,
+foto text,
+fk_unidade int);
+
+alter table Usuario add foreign key(fk_unidade) references Unidade(idUnidade);
 
 create table Sensor_Usuario(
 fk_Sensor int,
-Fk_CPF int);
+Fk_CPF bigint);
 
 alter table Sensor_Usuario add foreign key(fk_Sensor) references Sensor(idSensor);
 alter table Sensor_Usuario add foreign key(Fk_CPF) references Usuario(CPF);
+
+create table Sensor_Alerta(
+fk_Sensor int,
+fk_Alerta int);
+
+alter table Sensor_Alerta add foreign key(fk_Sensor) references Sensor(idSensor);
+alter table Sensor_Alerta add foreign key(fk_Alerta) references Alerta(idAlerta);
