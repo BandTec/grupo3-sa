f38a61ad62f91cd5fa3a6e088fa86957917f3e27
gustavouesso <gustavouesso.gu@gmail.com>
2019-05-29 20:41:14 -0300

    Selects

--- Projeto-Documentacao/Banco/script_selects.sql
+++ Projeto-Documentacao/Banco/script_selects.sql
@@ -11,13 +11,13 @@ select * from sensor as s,medicamentos as m where m.fk_sensor = s.idSensor
 
 select * from sensor as s,medicamentos as m where m.fk_Sensor = s.idSensor
 
-select avg(Temperatura_Atual);
+select avg(Temperatura_Atual) from Monitoramento;
 
 select (sum(Temperatura_Atual)/count(Temperatura_Atual)) from Monitoramento;
 
-select (count(Temperatura_Atual)+3)/4  order by Temperatura_Atual;  
+select (count(Temperatura_Atual)+3)/4  from Monitoramento order by Temperatura_Atual ;  
 
-select 3*(count(Temperatura_Atual)+1)/4  order by Temperatura_Atual;
+select  3*(count(Temperatura_Atual)+1)/4 from Monitoramento order by Temperatura_Atual;
 
 
 
