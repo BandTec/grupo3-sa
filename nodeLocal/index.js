d40aa5437cfc999a9dde653a248810195846b5f7
Segundo Usuario <ede>
2019-05-31 09:37:15 -0300

    PPT

--- nodeLocal/index.js
+++ nodeLocal/index.js
@@ -2,7 +2,7 @@ const SerialPort = require('serialport'); //Recupera o modulo Serial Port
 var controlle = require('./controller');
 var controller = new controlle();
 const Readline = SerialPort.parsers.Readline; // Atribui o metodo readline do serial port a variável ReadLine
-const port = new SerialPort('COM3'); //Conecta a porta serial COM5. Veja a sua na IDE do Arduino -> Tools -> Port
+const port = new SerialPort('COM8'); //Conecta a porta serial COM5. Veja a sua na IDE do Arduino -> Tools -> Port
    
 const parser = port.pipe(new Readline({delimiter: '\n'})); //Lê a linha apenas se uma nova linhas for inserida
 
